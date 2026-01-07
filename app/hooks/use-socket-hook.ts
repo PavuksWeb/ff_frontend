1c99b6f2cd3325a8e16421a24f324ca54e5b678c
PavuksWeb <112330889+PavuksWeb@users.noreply.github.com>
2026-01-07 18:11:20 +0200

    fix handle 429 error

--- app/hooks/use-socket-hook.ts
+++ app/hooks/use-socket-hook.ts
@@ -28,10 +28,6 @@ export function useSocket(url: string, onMessage: (msg: Message) => void) {
 
     socket.on('ws_error', (err) => {
       alert(err.message);
-      onMessage({
-        role: 'assistant',
-        message: '❌ The message has not passed moderation.',
-      });
     });
 
     socket.on('message_created', (data: Message) => {
